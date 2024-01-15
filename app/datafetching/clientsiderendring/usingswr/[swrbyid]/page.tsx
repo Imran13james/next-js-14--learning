f47b157178a09c0155c1c2b96411676d468635ba
Imranali <ia8214652@gmail.com>
2024-01-15 15:44:57 +0500

    cannot get the id using swr

--- app/datafetching/clientsiderendring/usingswr/[swrbyid]/page.tsx
+++ app/datafetching/clientsiderendring/usingswr/[swrbyid]/page.tsx
@@ -1,20 +1,17 @@
 'use client'
-
 import { useRouter } from "next/navigation";
 import useSWR from "swr";
-// Correct type for params
 interface RouteParams {
     usingswr: string;
-  }
+}
 const fetcher = (...args: [input: RequestInfo, init?: RequestInit | undefined]) => fetch(...args).then(res => res.json())
 
 export default function SWRgetbyid({ params }: { params: RouteParams }) {
     const router = useRouter()
     const id = params.usingswr;
-        console.log(id)
-        const URL = `https://jsonplaceholder.typicode.com/todos/${id}`
-    const { data, error } = useSWR(URL, fetcher);
+    const URL = `https://jsonplaceholder.typicode.com/todos/${id}`
 
+    const { data, error } = useSWR(URL, fetcher);
     if (error) {
         return <div> An error in useSWR</div>
     }
